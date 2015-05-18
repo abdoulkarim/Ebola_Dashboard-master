44ef91ab14326016f2f8c4bd5c72a018f7230ad2
abdoulkarim <abdoulkarimsow24@gmail.com>
2015-05-18 12:09:47 +0000

    Ebola Dashboard

---
+++ data/KeyStats.js
@@ -0,0 +1,6 @@
+var keyStats = 
+        {"Total":{"population":"21.7 mil"},
+	"Guinea":{"population":"11.5 mil"},
+	"Liberia":{"population":"4.2 mil",},
+	"Sierra Leone":{"population":"6.0 mil"},
+};		
